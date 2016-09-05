6db906ec98f91ee2bded70f9c52edaa7cc098175
TheNight <tarlis@mail.ru>
2016-09-05 05:25:58 +0500

    renamed

--- shortLinks/models.py
+++ shortLinks/models.py
@@ -3,11 +3,9 @@ from django.contrib.auth.models import User
 
 
 
-class Links(models.Model):
-    class Meta:
-        db_table = 'Links'
-    links_short = models.SlugField(max_length=50, primary_key=True)
-    links_http = models.URLField(max_length=255, default='')
-    links_count = models.IntegerField(default=0)
-    links_login = models.CharField(max_length=50, default='')
+class Link(models.Model):
+    short = models.SlugField(max_length=50)
+    full = models.URLField(max_length=4000)
+    count = models.IntegerField(default=0)
+    login = models.CharField(max_length=50)
 
