08b3df85996d2d1d57195fb2b35e66db379c811c
TheNight <tarlis@mail.ru>
2016-09-06 16:39:36 +0500

    auth in models

--- shortLinks/models.py
+++ shortLinks/models.py
@@ -1,10 +1,10 @@
+from django.conf import settings
 from django.db import models
 from django.contrib.auth.models import User
 
 
 class Link(models.Model):
-    short = models.SlugField(max_length=50)
+    short = models.SlugField(max_length=50, primary_key=True)
     full = models.URLField(max_length=4000)
     count = models.IntegerField(default=0)
-    login = models.CharField(max_length=50)
-
+    login = models.ForeignKey(settings.AUTH_USER_MODEL)
